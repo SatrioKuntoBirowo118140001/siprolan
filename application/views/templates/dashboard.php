b52df228069de74ef82c94b03ccee11d5e320814
118140001 - Satrio Kunto Birowo <satrio.118140001@student.itera.ac.id>
2021-05-18 09:07:50 +0700

    Update dashboard.php

--- application/views/templates/dashboard.php
+++ application/views/templates/dashboard.php
@@ -165,7 +165,6 @@
                                 <span class="mr-2 d-none d-lg-inline small text-capitalize">
                                     <?= userdata('nama'); ?>
                                 </span>
-                                <img class="img-profile rounded-circle" src="<?= base_url() ?>assets/img/avatar/<?= userdata('foto'); ?>">
                             </a>
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
