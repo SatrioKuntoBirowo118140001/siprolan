8c7164f38fc73987354dde6964002c4a501b9db5
ribkajsidabutar <ribkajsidabutar@gmail.com>
2021-05-18 09:36:11 +0700

    update database

--- sql/ci_barang.sql
+++ sql/ci_barang.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 18, 2021 at 04:18 AM
+-- Generation Time: May 18, 2021 at 04:34 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.4
 
